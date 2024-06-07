5061c15ba288b4e240bccdeb85ea3ebaf77e4226
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-06-07 21:10:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// let friends = ['mango' , ' poly ' , ' kiwi ' , ' ajax']
+// let string = friends.join(',')
+
+// console.log(string)
+
+// let friends = ['mango' , ' poly ' , ' kiwi ' , ' ajax']
+// for (let i = 0; i < friends.length; i++) {
+//   if (typeof friends[i] !== 'string') {
+//     friends[i] = '' + friends[i]; 
+//   }
+// }
+
+
+
+// console.log(friends)
+
+
+
+
+// let cards = [
+//     'Картка-1' , 
+//     'Картка-2' , 
+//     'Картка-3' , 
+//     'Картка-4' , 
+//     'Картка-5' , 
+// ]
+
+// let cardToRemove1 = cards.slice(0 , 2)
+// let cardToRemove2 = cards.slice(3 , 5)
+// console.log( joiner)
+
+
+
+// let cards = [
+//     'Картка-1' , 
+//     'Картка-2' , 
+//     'Картка-3' , 
+//     'Картка-4' , 
+//     'Картка-5' , 
+// ]
+// cards.push("картка-6")
+// console.log(cards)
+
+// let cards = [
+//     'Картка-1' , 
+//     'Картка-2' , 
+//     'Картка-3' , 
+//     'Картка-4' , 
+//     'Картка-5' , 
+// ]
+
+// let change1 = cards.splice(3 , 1 , "картка-100")
+// console.log(cards)
